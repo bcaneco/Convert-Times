7e38e664bf0bd5d8979be1b413828f3a9f74bbef
Bruno Caneco <bruno@dmpstats.com>
2024-03-07 15:11:05 +0000

    rFunction: update to move2
    Considerable re-coding including:
    - Sunset and sunrise computed using {suncalc} as {maptools} no longer active
    - `timestamp_local` now provided with correct timezones (before was incorrectly fixed to UTC)
    - column `timestamp_mean_solar` is now of POSIXct type
    - added handling of datasets with projected geometries and locations falling in geo-political disputed areas
    - columns `timestamp_local` and `local_tz` now included in output when `local_details==TRUE` while `local == FALSE`
    - code structure refactored with a dplyr-flavoured approach

--- RFunction.R
+++ RFunction.R
@@ -1,95 +1,159 @@
-library('move')
+library('move2')
 library('lubridate')
 library('lutz')
 library('sf')
-library('maptools')
+library('suncalc')
+library('dplyr')
+library('stringr')
+library('readr')
 
-rFunction <- function(data,local=FALSE,local_details=FALSE,sunriset=FALSE,mean_solar=FALSE,true_solar=FALSE)
+`%notin%` <- Negate(`%in%`)
+
+rFunction <- function(data,
+                      local=FALSE,
+                      local_details=FALSE,
+                      sunriset=FALSE,
+                      mean_solar=FALSE,
+                      true_solar=FALSE)
 {
-
-  Sys.setenv(tz="UTC")
   
-  if (local==FALSE & local_details==FALSE & sunriset==FALSE & mean_solar==FALSE & true_solar==FALSE) logger.info("You have not selected any timestamp conversion. The original dataset will be returned and provided as csv output.")
-  
-  data.csv <- as.data.frame(data)
-  names(data.csv) <- make.names(names(data.csv),allow_=FALSE)
-  location.long <- coordinates(data)[,1]
-  location.lat <- coordinates(data)[,2]
-  if (("individual.taxon.canonical.name" %in% names(data.csv))==FALSE & "taxon.canonical.name" %in% names(data.csv)) names(data.csv)[which(names(data.csv)=="taxon.canonical.name")] <- "individual.taxon.canonical.name"
-  if (("location.long" %in% names(data.csv))==FALSE) data.csv <- data.frame(data.csv,location.long,location.lat)
-  
-  other_names <- which(names(data.csv) %in% c("trackId","timestamp","location.long","location.lat","sensor","individual.taxon.canonical.name")==FALSE)
-  data.csv <- cbind(data.csv[c("trackId","timestamp","location.long","location.lat","sensor","individual.taxon.canonical.name")],data.csv[,other_names])
-
-  if (sunriset==TRUE)
-  {
-    logger.info("You have selected to add the time of sunrise and sunset.")
-    # add sunrise and sunset times of the day
-    sunrise_timestamp <- sunriset(coordinates(data),timestamps(data),direction="sunrise",POSIXct.out=TRUE)$time
-    sunset_timestamp <- sunriset(coordinates(data),timestamps(data),direction="sunset",POSIXct.out=TRUE)$time 
-    data@data <- cbind(data@data,sunrise_timestamp,sunset_timestamp)
-    data.csv <- cbind(data.csv,sunrise_timestamp,sunset_timestamp)
-  }
-  
-  # ask the cache and/or Google for the timezone at these coordinates
-  if (local==TRUE | local_details==TRUE) tz_info <- tz_lookup_coords(coordinates(data)[,2], coordinates(data)[,1], method = "accurate")
-
-  #return in daylight local
-  if(local==TRUE) 
-  {
-    logger.info("You have selected to add local timestamps.")
-    timestamp_local <- apply(data.frame(timestamps(data),tz_info), 1, function(x) as.character(lubridate::with_tz(x[1], x[2])))
-    timestamp_local[which(nchar(timestamp_local)==10)] <- paste(timestamp_local[which(nchar(timestamp_local)==10)],"00:00:00") #adapt midnight format
-    data@data <- cbind(data@data,timestamp_local,"local_tz"=tz_info)
-    data.csv <- cbind(data.csv,timestamp_local,"local_timezone"=tz_info)
-  }
-  
-  if (local_details==TRUE)
-  {
-    logger.info("You have selected to add detailed time information of the local timestamps.")
-    timestamp_local <- apply(data.frame(timestamps(data),tz_info), 1, function(x) as.character(lubridate::with_tz(x[1], tzone=x[2])))
-    timestamp_local[which(nchar(timestamp_local)==10)] <- paste(timestamp_local[which(nchar(timestamp_local)==10)],"00:00:00") #adapt midnight format
-    timestamp_local_PX <- as.POSIXct(timestamp_local,format="%Y-%m-%d %H:%M:%S") #note that UTC here is technically not correct, but this way can calculate local time details
-    date <- as.Date(timestamp_local_PX)
+  if (sum(local, local_details, sunriset, mean_solar, true_solar) == 0){
     
-    time <- strftime(timestamp_local_PX, format="%H:%M:%S")
-    year <- year(timestamp_local_PX)
-    month <- months(as.POSIXct(timestamp_local_PX))
-    yday <- as.numeric(strftime(timestamp_local_PX, format = "%j"))
-    calender_week <- as.numeric(strftime(timestamp_local_PX, format = "%V"))
-    weekday <- weekdays(as.POSIXct(timestamp_local_PX))
-    data@data <- cbind(data@data,date,time,year,month,weekday,yday,calender_week)
-    data.csv <- cbind(data.csv,date,time,year,month,weekday,yday,calender_week)
-  }
-
-  if (mean_solar==TRUE)
-  {
-    logger.info("You have selected to add mean solar time timestamps.")
-    # reference to convert_UTC_to_solartime function by Alison Appling (USGS)
-    time.adjustment <- 239.34 * coordinates(data)[,1] #in secs --> 3.989 minutes = 239.34 seconds per degree
-    timestamp_mean_solar <- as.character(timestamps(data) + as.difftime(time.adjustment,units="secs")) #add seconds to the UTC time
-    data@data <- cbind(data@data,timestamp_mean_solar)
-    data.csv <- cbind(data.csv,timestamp_mean_solar)
-  }
-  if (true_solar==TRUE)
-  {
-    logger.info("You have selected to add true solar time timestamps.")
-    time.adjustment <- 239.34 * coordinates(data)[,1] #in secs --> 3.989 minutes = 239.34 seconds per degree
-    timestamp_mean_solar <- timestamps(data) + as.difftime(time.adjustment,units="secs") #add seconds to the UTC time
-    # Use the equation of time to compute the discrepancy between apparent and
-    # mean solar time. E is in minutes.
-    jday <- as.numeric(strftime(timestamp_mean_solar, format = "%j")) -1
-    E <- 9.87*sin(((2*360*(jday-81))/365)/180*pi) - 7.53*cos(((360*(jday-81))/365)/180*pi) - 1.5*sin(((360*(jday-81))/365)/180*pi)
-    timestamp_true_solar <- timestamp_mean_solar + as.difftime(E,units="mins")
-    data@data <- cbind(data@data,timestamp_true_solar)
-    data.csv <- cbind(data.csv,timestamp_true_solar)
-  }    
-
-  write.csv(data.csv, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"data_wtime.csv"),row.names=FALSE)
-  #write.csv(data.csv, file = "data_wtime.csv",row.names=FALSE)
+    logger.info("You have not selected any timestamp conversion. The original dataset will be returned and provided as csv output.")
+    
+    readr::write_csv(data, file = appArtifactPath("data_wtime.csv"))
+    
+  } else {
   
-  result <- data
-  return(result)
+    #' get longlat coords, transforming accordingly if CRS is not WGS84
+    if(!st_is_longlat(data)){
+      lonlat <- data |> 
+        sf::st_transform(4326) |> 
+        sf::st_coordinates()
+    }else{
+      lonlat <- sf::st_coordinates(data)
+    }
+    
+    # get the time column of input dataset
+    tm_col_id <- mt_time_column(data)  
+    
+    
+    # Add sunrise and sunset timestamps
+    if(sunriset == TRUE){
+      logger.info("You have selected to add the time of sunrise and sunset.")
+      
+      # Calculate UTC sunrise and sunset for each event/location
+      sun_rise_set <- data.frame(
+        date = as.Date(data[[tm_col_id]]),
+        lon = lonlat[, "X"],
+        lat = lonlat[, "Y"]
+      ) |> 
+        suncalc::getSunlightTimes(data = _, keep = c("sunrise", "sunset"), tz = "UTC")
+      
+      # bind new cols
+      data <- data |> 
+        dplyr::mutate(
+          sunrise_timestamp = sun_rise_set$sunrise,
+          sunset_timestamp = sun_rise_set$sunset
+        )
+    }
+    
+    
+    # Bind local time variables
+    if(local == TRUE | local_details == TRUE) {
+      
+      if(local == TRUE) logger.info("You have selected to add local timestamps.") 
+      
+      # add timezone - ask the cache and/or Google for the timezone at these coords
+      data$local_tz <- lutz::tz_lookup_coords(lonlat[, "Y"], lonlat[, "X"], method = "accurate")
+      
+      # handling events with multiple TZs due to geo-political disputes
+      multz_idx <- stringr::str_which(data$local_tz, ".+;.+") # {lutz} separates TZs with ';'
+      
+      if(length(multz_idx) > 0){
+        logger.warn(
+          paste0(
+            "Some events initially annotated with multiple TZs for being located ",
+            "in areas under geo-political disputes.\n",
+            "      Selecting the first TZ provided for timestamp parsing purposes.")
+        )
+        
+        data$local_tz[multz_idx] <- stringr::str_extract(data$local_tz[multz_idx], ".+(?=;)")
+      }
+      
+      # add local timestamps
+      data <- data |>
+        dplyr::rowwise() |> 
+        dplyr::mutate(
+          timestamp_local = lubridate::with_tz(.data[[tm_col_id]], local_tz),
+          .before = local_tz
+        ) |> 
+        dplyr::ungroup()
+      
+      
+      # add local time details
+      if (local_details == TRUE){
+        logger.info("You have selected to add detailed time information of the local timestamps.")
+        
+        data <- data |> 
+          dplyr::mutate(
+            date = as.Date(timestamp_local),
+            time = format(timestamp_local, format="%H:%M:%S"),
+            year = lubridate::year(timestamp_local),
+            month = months(timestamp_local),
+            weekday = weekdays(timestamp_local),
+            yday = as.numeric(format(timestamp_local, format = "%j")),
+            calender_week = as.numeric(format(timestamp_local, format = "%V"))
+          )
+      }
+    }
+    
+    
+    if(mean_solar == TRUE | true_solar == TRUE){
+      
+      # reference to convert_UTC_to_solartime function by Alison Appling (USGS)
+      time.adjustment <- 239.34 * lonlat[, "X"] #in secs --> 3.989 minutes = 239.34 seconds per degree
+      
+      # add mean solar timestamp (ensuring computations based on UTC)
+      timestamp_mean_solar <- lubridate::with_tz(data[[tm_col_id]], "UTC") + as.difftime(time.adjustment, units="secs")
+      
+      if(mean_solar == TRUE){
+        logger.info("You have selected to add mean solar time timestamps.")  
+        data$timestamp_mean_solar <- timestamp_mean_solar
+      }
+      
+      if(true_solar == TRUE){
+        logger.info("You have selected to add true solar time timestamps.")
+        
+        # Use the equation of time to compute the discrepancy between apparent and
+        # mean solar time. E is in minutes.
+        jday <- as.numeric(format(timestamp_mean_solar, format = "%j")) - 1
+        E <- 9.87*sin(((2*360*(jday-81))/365)/180*pi) - 7.53*cos(((360*(jday-81))/365)/180*pi) - 1.5*sin(((360*(jday-81))/365)/180*pi)
+        timestamp_true_solar <- timestamp_mean_solar + as.difftime(E, units="mins")
+        
+        data$timestamp_true_solar <- timestamp_true_solar
+      }
+    }
+    
+    
+    # Prepare data for csv artifact (grossly based on previous app move1 version)
+    data |> 
+      dplyr::relocate(
+        mt_track_id_column(data),
+        dplyr::all_of(tm_col_id),
+        .before = 1
+      ) |> 
+      dplyr::mutate(
+        location_long = lonlat[, "X"], 
+        location_lat = lonlat[, "Y"],
+        .after = dplyr::all_of(tm_col_id)
+      ) |> 
+      # using {readr} as `write.csv` was not exporting the data correctly
+      readr::write_csv(file = appArtifactPath("data_wtime.csv"))
+  }
+  
+  return(data)
+  
 }
 
   
